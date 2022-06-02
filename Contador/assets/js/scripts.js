c7716fcc855c82b18c5d64bdc8494c41ca3aa5da
Letícia <leticiatrindade0504@gmail.com>
2022-06-02 13:39:09 -0300

    Segundo commit

--- Contador/assets/js/scripts.js
+++ Contador/assets/js/scripts.js
@@ -4,7 +4,6 @@ var currentNumberWhapper = document.getElementById("currentNumber");
 let button = document.getElementById("add");
 let button2 = document.getElementById("sub");
 
-
 console.log(typeof button);
 console.log(typeof currentNumberWhapper);
 
@@ -13,9 +12,12 @@ function increment(){
     currentNumberWhapper.firstChild.nodeValue = currentNumberWhapper.innerText++;
     if ( currentNumberWhapper.innerText > 9){
         button.disabled = true;
+        currentNumberWhapper.style.color = "red";
+
     }
     else {
         button.disabled = false;
+        currentNumberWhapper.style.color = "";
     }
 
     console.log(currentNumberWhapper.innerText);
@@ -26,12 +28,23 @@ function decrement(){
     currentNumberWhapper.firstChild.nodeValue = currentNumberWhapper.innerText--;
     if ( currentNumberWhapper.innerText < 1){
         button2.disabled = true;
+        currentNumberWhapper.style.color = "red";
     }
     else{
         button2.disabled = false;
+        currentNumberWhapper.style.color = "black";
     }
 
     console.log(currentNumberWhapper.innerText);
+}
+
+function reset(){
+    button.disabled = false;
+    button2.disabled = false;
+    currentNumberWhapper.innerText = 0;
+    currentNumberWhapper.style.color = "black";
+    
+
 }
 
 var test = document.getElementById("sub");
@@ -39,4 +52,7 @@ test.addEventListener("click", decrement, false);
 
 var test = document.getElementById("add");
 test.addEventListener("click", increment, false); 
+
+var test = document.getElementById("reset");
+test.addEventListener("click", reset, false); 
 
